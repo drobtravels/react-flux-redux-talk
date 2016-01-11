e1bb95ef83452666c2a523109209086b78dce4d4
David Roberts <drob@drob.net>
2016-01-10 19:36:21 -0500

    Initial react skeleton

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Calls Application </h1>
+      </div>
+    );
+  }
+}
